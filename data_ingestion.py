9be985fe09de6d45615e7495fdb0658cc2b2ca0c
Nisarg <nisarg7105@gmail.com>
2026-06-03 00:09:15 +0530

    Day 1: ingestion completed

---
+++ data_ingestion.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+folder = "data/raw"
+
+for file in os.listdir(folder):
+    if file.endswith(".csv"):
+        print("\n" + "="*50)
+        print("FILE:", file)
+
+        df = pd.read_csv(os.path.join(folder, file))
+
+        print("Shape:", df.shape)
+        print("\nData Types:")
+        print(df.dtypes)
+        print("\nHead:")
+        print(df.head())
